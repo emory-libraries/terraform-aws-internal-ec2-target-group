179e9a001c776514bd834b1d8f49c9935fa0a0a5
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-12 13:51:13 -0500

    Add target group logic

---
+++ target-group-listener-rule.tf
@@ -0,0 +1,38 @@
+resource "aws_alb_target_group" "target_group" {
+  name     = "${local.namespace}-alb-web"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 1800
+    enabled         = "true"
+  }
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    port                = "80"
+  }
+}
+
+
+# Web listener rule and target group
+resource "aws_lb_listener_rule" "alb_web_listener_rule" {
+  listener_arn = aws_alb_listener.alb_listener.arn
+  priority     = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.target_group.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.appended_fqdn]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "application_fqdn" {
+  type = string
+  description = <<EOF
+    The fully qualified production domain name. This is name is used only by the application load balancer, not route53.
+    Note that the template will also create another domain name for streaming that is streaming.{application_fqdn}.
+    EOF
+}
+variable "application_fqdn_workspace_insertion_index" {
+  type = number
+  default = 0
+  description = <<EOF
+    The application fqdn is split into a list at each '.', this variable is the index (first object is 0) where the workspace will be appended.
+    For example if the application fqdn is 'avr.emory.edu', this variable is set to 0, and the workspace is test, the output will be avr-test.emory.edu.
+    If the workspace is 'prod' then nothing is appended to the fqdn and the address on the alb would be 'avr.emory.edu'. 
+    EOF
+}
