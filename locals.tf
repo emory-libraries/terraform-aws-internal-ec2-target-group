98b9d5956dd6cc873028e5c1fd96f9994f717975
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-12 13:52:57 -0500

    Add local functions

--- locals.tf
+++ locals.tf
@@ -2,5 +2,4 @@ locals {
     application_fqdn_list = split(".", var.application_fqdn)
     appended_fqdn_list = terraform.workspace == "prod" ? local.application_fqdn_list : formatlist("%s-${terraform.workspace}", local.application_fqdn_list)
     appended_fqdn = replace(var.application_fqdn, local.application_fqdn_list[var.application_fqdn_workspace_insertion_index], local.appended_fqdn_list[var.application_fqdn_workspace_insertion_index])
-
 }
